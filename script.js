434945a2c0daab98bbe3b890647ac6f93618379e
XxPedroAlmeidaxX <pedrohenriquealmeidacosta@gmail.com>
2019-05-19 13:52:51 -0300

    Finishing dynamic news implementation

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-function getNews(category, callback) {
-    let endpoint = `https://newsapi.org/v2/top-headlines?country=br&category=${category}&apiKey=${apiKey}`;
-
-    fetch(endpoint).then(response => {
-        return response.json();
-    }).then(data => {
-        return data;
-    }).catch(err => {
-        console.log(err);
-    }).then(data => {
-        //noinspection JSUnresolvedVariable
-        let news = data.articles.slice(0, 5);
-        saveNews(category, news);
-        callback(news);
-    });
-}
-
-function saveNews(category, newsToSave) {
-    let news = JSON.parse(localStorage.getItem("news"));
-    newsToSave.forEach(item => {
-        item.uuid = createUUID();
-    })
-    news[category] = newsToSave;
-    localStorage.setItem("news", JSON.stringify(news));
-}
-
-function createUUID() {
-    let dt = new Date().getTime();
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        let r = (dt + Math.random() * 16) % 16 | 0;
-        dt = Math.floor(dt / 16);
-        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
-}
-
 function setNews() {
     localStorage.setItem("news", "{}");
 
@@ -61,17 +26,86 @@ function setNews() {
     });
 }
 
+function getNews(category, callback) {
+    let endpoint = `https://newsapi.org/v2/top-headlines?country=br&category=${category}&apiKey=${apiKey}`;
+
+    fetch(endpoint).then(response => {
+        return response.json();
+    }).then(data => {
+        return data;
+    }).catch(err => {
+        console.log(err);
+    }).then(data => {
+        //noinspection JSUnresolvedVariable
+        let news = data.articles.slice(0, 5);
+        saveNews(category, news);
+        callback(news);
+    });
+}
+
+function saveNews(category, newsToSave) {
+    let news = JSON.parse(localStorage.getItem("news"));
+    newsToSave.forEach(item => {
+        item.category = category;
+        item.uuid = createUUID();
+    });
+    news[category] = newsToSave;
+    localStorage.setItem("news", JSON.stringify(news));
+}
+
+function createUUID() {
+    let dt = new Date().getTime();
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
 function renderNews(news, list) {
     news.forEach(item => {
-        list.innerHTML += `<li><a href='noticia.html?article_id=${item.uuid}'>${item.title}</a></li>`;
-    })
+        list.innerHTML += `<li><a href='noticia.html?article_id=${item.uuid}&category=${item.category}'>${item.title}</a></li>`;
+    });
     list.innerHTML += `<li><a href='#'>Mais notícias...</a></li>`;
 }
 
-
 function setArticle() {
-    let articleId = getUrlParams(window.location.href);
-    console.log(articleId)
+    let params = getUrlParams(window.location.href);
+    let news = JSON.parse(localStorage.getItem("news"));
+
+    news = news[params.category];
+    let article = news.find(item => {
+        //noinspection JSUnresolvedVariable
+        if (item.uuid === params.article_id)
+            return item;
+    });
+
+    renderArticle(article);
+}
+
+function renderArticle(article) {
+    let articleElement = document.getElementById("articleElement");
+    //noinspection JSUnresolvedVariable
+    articleElement.innerHTML =
+        `<header>
+            <h1>${article.title}</h1>
+            <p>${article.author || article.source.name}, 
+                <time datetime='${article.publishedAt}'>${formatDate(article.publishedAt)}</time>
+            </p>
+         </header>
+         
+         <figure>
+            <img src='${article.urlToImage || ""}' alt="Imagem da Notícia"/>
+         </figure>
+         
+         <p>${article.description || ""}</p>
+         <p>${article.content || ""}</p>
+         <p>Source: ${article.url}</p>`;
+}
+
+function formatDate(dateToFormat) {
+    let date = new Date(dateToFormat);
+    return date.toLocaleDateString('pt-br', {timeZone: 'UTC'});
 }
 
 function getUrlParams(url) {
