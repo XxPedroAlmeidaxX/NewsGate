145363e4ddab52a173a8840b7eaa1685dd5b4430
XxPedroAlmeidaxX <pedrohenriquealmeidacosta@gmail.com>
2019-05-19 03:09:31 -0300

    Building article page

--- script.js
+++ script.js
@@ -8,41 +8,81 @@ function getNews(category, callback) {
     }).catch(err => {
         console.log(err);
     }).then(data => {
-        callback(data);
+        //noinspection JSUnresolvedVariable
+        let news = data.articles.slice(0, 5);
+        saveNews(category, news);
+        callback(news);
     });
 }
 
+function saveNews(category, newsToSave) {
+    let news = JSON.parse(localStorage.getItem("news"));
+    newsToSave.forEach(item => {
+        item.uuid = createUUID();
+    })
+    news[category] = newsToSave;
+    localStorage.setItem("news", JSON.stringify(news));
+}
+
+function createUUID() {
+    let dt = new Date().getTime();
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
 
 function setNews() {
-    getNews("technology", data => {
-        render(data, document.getElementById("technologyList"));
+    localStorage.setItem("news", "{}");
+
+    getNews("technology", news => {
+        renderNews(news, document.getElementById("technologyList"));
     });
 
-    getNews("entertainment", data => {
-        render(data, document.getElementById("entertainmentList"));
+    getNews("entertainment", news => {
+        renderNews(news, document.getElementById("entertainmentList"));
     });
 
-    getNews("business", data => {
-        render(data, document.getElementById("businessList"));
+    getNews("business", news => {
+        renderNews(news, document.getElementById("businessList"));
     });
 
-    getNews("science", data => {
-        render(data, document.getElementById("scienceList"));
+    getNews("science", news => {
+        renderNews(news, document.getElementById("scienceList"));
     });
 
-    getNews("health", data => {
-        render(data, document.getElementById("healthList"));
+    getNews("health", news => {
+        renderNews(news, document.getElementById("healthList"));
     });
 
-    getNews("sports", data => {
-        render(data, document.getElementById("sportsList"));
+    getNews("sports", news => {
+        renderNews(news, document.getElementById("sportsList"));
     });
 }
 
-function render(data, list) {
-    for (let i = 0; i < 5; i++) {
-        //noinspection JSUnresolvedVariable
-        list.innerHTML += `<li><a href='noticia.html'>${data.articles[i].title}</a></li>`;
-    }
+function renderNews(news, list) {
+    news.forEach(item => {
+        list.innerHTML += `<li><a href='noticia.html?article_id=${item.uuid}'>${item.title}</a></li>`;
+    })
     list.innerHTML += `<li><a href='#'>Mais notícias...</a></li>`;
+}
+
+
+function setArticle() {
+    let articleId = getUrlParams(window.location.href);
+    console.log(articleId)
+}
+
+function getUrlParams(url) {
+    let params = {};
+    let parser = document.createElement('a');
+    parser.href = url;
+    let query = parser.search.substring(1);
+    let vars = query.split('&');
+    for (let i = 0; i < vars.length; i++) {
+        let pair = vars[i].split('=');
+        params[pair[0]] = decodeURIComponent(pair[1]);
+    }
+    return params;
 }
